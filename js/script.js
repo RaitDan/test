4296b851a9152f7c6f79b586b600faec79061c73
Rait <dan.zrayt@gmail.com>
2022-09-16 19:08:40 +0300

    lesson

--- js/script.js
+++ js/script.js
@@ -1,23 +1,40 @@
 'use strict';
 
-const number0Films = +prompt('Сколько фильмов вы уже посмотрели?', '');
- 
-const personalMovieDB = {
-    count: number0Films,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-}; 
+// if (4 == 9) {
+//     console.log('Ok!');
+// } else {
+//     console.log("FFFF");
+// }
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''), 
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', ''); 
+// const num = 50;
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// if (num < 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log('Много');
+// } else {
+//     console.log('Ok!');
+// }
 
-console.log(personalMovieDB)
+// (num === 50) ? console.log('Ok!') : console.log('Error'); 
+
+const num = 50;
+
+switch (num ) {
+    case 49:
+        console.log('Неверно');
+         break;
+    case 100:
+        console.log('Неверно');
+         break;
+    case 5пше0:
+        console.log('Верно');
+         break;
+    default:
+        console.log('Не в этот раз');
+         break;   
+}
+
+
 
 
