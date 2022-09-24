a304890cd45d8fe71c78c630694fc3c60d3814e6
Rait <dan.zrayt@gmail.com>
2022-09-24 17:10:14 +0300

    Object obj

--- js/script.js
+++ js/script.js
@@ -1,15 +1,35 @@
 'use strict';
 
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red',
+    },
+    makeTest: function() {
+        console.log("Test")
+    }
+};
 
-function calcSumm (numOne = 1, numTwo = 3) {
-    console.log(`Переменная numOne: ${numOne}`);
-    console.log(`Перемення numTwo: ${numTwo}`);
+options.makeTest();
 
-    let numSumm = numOne + numTwo;
+console.log(Object.keys(options).length);
 
-    console.log(`Сумма: ${numSumm}`);
-}
+console.log(options.name);
 
-calcSumm();
- 
+ delete options.name;
 
+ console.log(options);
+
+ for (let key in options) {
+    if (typeof(options[key]) === 'object') {
+        for (let i in options[key]) {
+            console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+        }
+    } else {
+        console.log(`Свойство ${key} имеет значение ${options[key]}`);
+    }
+ }
+  
