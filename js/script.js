719e2547012023fb98f04e723ed3d855256d6914
Rait <dan.zrayt@gmail.com>
2022-10-03 15:14:55 +0300

    object assign

--- js/script.js
+++ js/script.js
@@ -1,27 +1,126 @@
-'use strict';
+"use strict";
 
-const arr = [1, 2, 3, 6, 8];
+// let a = 5,
+//     b = a;
 
-// arr.pop();
+// b = b + 5;
 
-// arr.push();
+// console.log(b);
+// console.log(a);
 
-// console.log(arr);
-  
-// arr.forEach(function(item, i, arr) {
-//     console.log(`${i}: ${item} внутри массива ${arr}`) 
-// });
+// const obj = {
+//     a: 5,
+//     b: 1
+// };
 
+// // const copy = obj;
 
-// for (let i = 0; i < arr.length; i++){
-//      console.log(arr[i])
+// // copy.a = 10;
+
+// // console.log(copy);
+// // console.log(obj);
+
+// function copy(mainObj){
+//     let objCopy = {};
+
+//     let key;
+//     for (key in mainObj) {
+//          objCopy[key] = mainObj[key]
+//     }
 // }
 
-// for (let value of arr) {
-//     console.log(value);
-// } 
 
-const str = prompt("", "");
-const products = str.split(", ");
-products.sort();
-console.log(products.join('; '));
+
+
+
+// let userInfo = {
+//     name: "Вася",
+//     age: 30,
+// }
+
+
+// let user = Object.assign({}, userInfo);
+
+// user.age = 18;
+// userInfo.name = "Даша";
+
+// console.log(userInfo);
+// console.log(user);
+
+
+// let userWeed = {
+//     name: "Даня",
+//     age: 20,
+// }
+
+// Object.assign(userWeed, {['Likes Weed']: true, city: "Chisinau"});
+// console.log(userWeed);
+
+
+
+// let personeInfo = {
+//     name: "Шмаль",
+//     age: 2,
+//     adress: {
+//         city: "Chisinau",
+//         street: "Freedom",
+//     }
+// }
+
+// for (let key in personeInfo) {
+//     //Ключи
+//     console.log(key);
+//     //Значения ключей
+//     console.log(personeInfo[key]);
+// }
+
+// for (let key in personeInfo.adress) {
+//     //Ключи
+//     console.log(key);
+//     //Значения ключей
+//     console.log(personeInfo.adress[key]);
+// }
+
+
+//Дублирование объектов
+const obj = {
+    a: 5,
+    b: 1
+};
+
+// const copy = Object.assign({},obj);
+
+// copy.a = 10;
+// copy.b = 15 - 6;
+
+// console.log(obj);
+// console.log(copy); 
+
+function copy(mainObj) {
+    let objCopy = {};
+
+    let key;
+    for (key in mainObj) {
+        objCopy[key] = mainObj[key];
+    }
+}
+
+
+let num = 50;
+
+for (let i = 1; i < 10; i++ ) {
+    if (i === 6) {
+        break;
+    }
+
+    console.log(i);
+}
+
+
+
+
+
+
+
+
+
