bbee3be85d89edcb4b74365683c526aef99110b5
Rait <dan.zrayt@gmail.com>
2022-10-12 04:40:12 +0300

    hwfrlinals

--- js/script.js
+++ js/script.js
@@ -1,12 +1,33 @@
 "use strict";
 
-let str = "some";
-let strObj = new String(str);
+let users = ['Ваня', 'Иштван',];
 
-console.log(typeof(str));
-console.log(typeof(strObj));
+users.push('Оля');
+console.log(users);
+
+users.splice(1, 1, 'Петя');
+console.log(users);
+
+let oneUsers = users.splice(0, 1);
+console.log(oneUsers);
+console.log(users);
+
+users.splice(0, 0, 'Маша', 'Паша');
+console.log(users);
 
 
+let arr = ['Ваня', 'Иштван', 'Оля',];
+
+let arrTwo = arr.splice(1, 1);
+
+console.log(arr);
+console.log(arrTwo);
+
+let str = 'Ваня,Иштван,Оля';
+
+let newStr = str.split(',');
+
+console.log(newStr);
 
 
 
